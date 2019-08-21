81a859a847a98861816ba27f21245dc29ec1d58c
Bryan Clement <bclement@newrelic.com>
2019-08-20 21:20:14 -0700

    spit out which files load a given file

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #! /usr/bin/env node
-
 'use strict'
 
-const loader = require('./loader')
+const path = require('path')
 
-var graph = loader.loadAndParseFromPath(process.argv[2])
-console.log(Object.keys(graph))
+const loader = require('./loader')
+const loadGraph = loader.loadAndParseFromPath(path.resolve('.', process.argv[2]))
+const paths = Object.keys(loadGraph)
+
+const needle = path.resolve('.', process.argv[3])
+const containingNeedle = []
+for (let path of paths) {
+  if (loadGraph[path].map(n => n.path).includes(needle)) {
+    containingNeedle.push(path)
+  }
+}
+
+console.log(containingNeedle)
--- loader/index.js
+++ loader/index.js
@@ -7,18 +7,6 @@ const fs = require('fs')
 const es = require('esprima')
 
 const util = require('../util')
-const typer = require('../typer')
-const callGrapher = require('../callGraph')
-const { functionTypes, returnTypes, callTypes } = require('../tokenTypes')
-
-// Keys we will be defining on the tokens as we iterate through them
-const ourKeys = new Set([
-  'mutates',
-  'declares',
-  'calls',
-  'parent',
-  'returns'
-])
 
 // Returns an object of the AST and filename of a file
 function getLoadNode(filename, parent) {
@@ -30,11 +18,11 @@ function getLoadNode(filename, parent) {
       Could not find package ${filename}
     `)
     return {
-      path: 'not found'
+      path: null
     }
   }
 
-  var source = null
+  let source = null
   if (path.extname(moduleFilename) === '.js') {
     source = es.parse(
       util.stripShebang(
@@ -51,46 +39,24 @@ function getLoadNode(filename, parent) {
 
 // Parse the AST and pull out the data we are interested in
 function getMetaData(rootToken) {
-  var loadPaths = []
-  var functionTokens = []
+  const loadPaths = []
 
   if (rootToken != null) {
-    var toRelax = [rootToken]
+    const toRelax = [rootToken]
 
     while (toRelax.length) {
-      var currentToken = toRelax.pop()
-
-      const type = currentToken.type
-      if (functionTypes.has(type)) {
-        // Collect function tokens for later parsing
-        functionTokens.push(currentToken)
-        currentToken.calls = []
-        currentToken.returns = []
-      } else if (returnTypes.has(type)) {
-        // Parse return statements immediately
-        typer.parseReturnToken(currentToken)
-      } else if (callTypes.has(type)) {
-        // Parse call statements immediately
-        callGrapher.parseCallToken(currentToken)
-      } else if (type === 'BlockStatement') {
-        typer.parseBlockToken(currentToken)
+      const currentToken = toRelax.pop()
+      if (!currentToken) {
+        console.log(currentToken)
+        continue
       }
-
       // Push nodes onto the stack and annotate nodes with their parents
       for (let key of Object.keys(currentToken)) {
-        if (ourKeys.has(key)) {
-          continue
-        }
-
-        var value = currentToken[key]
+        const value = currentToken[key]
         if (value && value instanceof Object) {
           if (Array.isArray(value)) {
-            for (let t of value) {
-              t.parent = currentToken
-            }
             Array.prototype.push.apply(toRelax, value)
           } else {
-            value.parent = currentToken
             toRelax.push(value)
           }
         }
@@ -105,45 +71,41 @@ function getMetaData(rootToken) {
   }
 
   return {
-    loadPaths,
-    functionTokens
+    loadPaths
   }
 }
 
 function parseProgram(root) {
-  var loads = {}
-  var toRelax = [root]
+  const loads = {}
+  const toRelax = [root]
   while (toRelax.length) {
-    var currentToken = toRelax.pop()
+    const currentToken = toRelax.pop()
 
-    if (!loads[currentToken.path]) {
-      var tokenMeta = getMetaData(currentToken.source)
-      var fileLoads = tokenMeta.loadPaths
-      currentToken.functionTokens = tokenMeta.functionTokens
+    const tokPath = currentToken.path
+    if (tokPath && !loads[tokPath]) {
+      const { loadPaths } = getMetaData(currentToken.source)
 
-      loads[currentToken.path] = fileLoads.map(function(path) {
+      const nodes = loads[tokPath] = loadPaths.map(function(path) {
         return getLoadNode(path, currentToken.path)
       })
 
-      Array.prototype.push.apply(
-        toRelax,
-        loads[currentToken.path]
-      )
+      toRelax.push(...nodes)
     }
   }
   return loads
 }
 
 function getRequirePath(token) {
-  if (token.type === 'CallExpression' && token.callee.name === 'require') {
+  if (token.type === 'CallExpression' && token.callee.name === 'require' && token.arguments[0].type === 'Literal') {
     return token.arguments[0].value
   }
 }
 
 module.exports = {
   loadAndParseFromPath: function loadAndParseFromPath(entryPath) {
+    let root
     try {
-      var root = getLoadNode(entryPath, '.')
+      root = getLoadNode(entryPath, '.')
     } catch (e) {
       // TODO: more graceful error handling when this is used for something
       console.log('could not load ' + entryPath)
